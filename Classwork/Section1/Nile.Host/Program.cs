1e93993cefd5fa005f7a0cc7175b75b2da5f0ae3
david.keeton821 <david.keeton821@my.tccd.edu>
2018-01-24 21:41:45 -0600

    Classwork on data types

---
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+        }
+
+        static void PlayingWithPrimitives()
+        {
+            //Primitive
+            decimal unitPrice = 10.5M;
+
+            //Real Declaration
+            Decimal unitPrice2 = 10.5M;
+
+            //Current Time
+            DateTime now = DateTime.Now;
+
+            ArrayList items;
+        }
+
+        static void PlayingWithVariables ()
+        {
+
+            {
+                //single declarations
+                int hours = 0;
+                double rate = 10.25;
+
+                // still not assigned
+                // if (false)
+                //     hours = 0;
+                int hours2 = hours;
+
+                //multiple declarations
+                string firstName, lastName;
+
+                //single assignment
+                firstName = "Bob";
+                lastName = "Miller";
+
+                //multiple assignment
+                firstName = lastName = "Sue";
+
+                //math operators
+                int x = 0, y = 10;
+                int add = x + y;
+                int subtract = x - y;
+                int multiply = x * y;
+                int divide = x / y;
+                int modulos = x % y;
+
+                //x = x + 10
+                x += 10;
+                double ceiling = Math.Ceiling(rate);
+                double floor = ceiling;
+
+            }
+        }
+    }
+}
