d321169fb41362aac05bfe33b765b37e85c606b4
David Keeton <david.keeton821@my.tccd.edu>
2018-01-25 22:22:00 -0600

    Pseudocode to map out project

---
+++ Labs/Lab1/DavidKeeton.MovieLib.Host/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavidKeeton.MovieLib.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            //Main Menu
+            //List the movies
+            //Add a new movie
+            //Remove a Movie
+            //Exit
+
+        }
+
+        //Function: Add a movie
+            //Gather Movie variables
+            //Title: Text, requried
+            //Description: Text, Optional
+            //Length: Int, Optional
+            //Owned: Boolean, Required
+        
+        //Function: List the Movies
+
+        //Remove a Movie
+    }
+}
