bf0942539e152c263dce9c9d0500d6492998c713
david.keeton821 <david.keeton821@my.tccd.edu>
2018-04-25 21:10:10 -0500

    Added in productscontroller functionality

---
+++ Classwork/Section5/Nile.Web.Mvc/Models/ProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Web.Mvc.Models
+{
+    /// <summary>Provides information about a product. </summary>
+    public class ProductModel
+    {
+        
+        public int Id { get; set; }
+      
+        // value tag allows documenting default value if not "0"
+        public string Description { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        [RangeAttribute(0, Double.MaxValue, ErrorMessage = "Price must be >= 0")]
+        public decimal Price { get; set; }
+
+        public bool IsDiscontinued { get; set; }
+    }
+}
